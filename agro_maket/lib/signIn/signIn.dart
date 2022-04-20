f1011466dd2e00736bc25cb72a61f382a205a179
ABDOULHAMID Mhadjou <abdoulhamid@iut-dhaka.edu>
2022-04-20 23:02:14 +0600

    Update signIn.dart

--- agro_maket/lib/signIn/signIn.dart
+++ agro_maket/lib/signIn/signIn.dart
@@ -13,21 +13,23 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-       // height: double.infinity,
-       child: Column(
-         children: [
-           SizedBox(height: 120,), // figma height:63
-           Logo(),
-           SizedBox(height: 58,),
-           Connexion(),
-           SizedBox(height: 27,),
-           forgetPassword(),
-           SizedBox(height: 40,),
-           SignUp(),
-         ],
-       ),
+      body:SafeArea(
+        child: Container(
+          width: double.infinity,
+         // height: double.infinity,
+         child: Column(
+           children: [
+             SizedBox(height: 120,), // figma height:63
+             Logo(),
+             SizedBox(height: 58,),
+             Connexion(),
+             SizedBox(height: 27,),
+             forgetPassword(),
+             SizedBox(height: 40,),
+             SignUp(),
+           ],
+         ),
+        ),
       )
     );
   }
